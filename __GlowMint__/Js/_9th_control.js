c9cc5c925cfc7fa5e5af6a11ba2846910a4c3424
Co786-stars <raditya82453@outlook.com>
2026-09-12 08:31:19 +0530

    difining the loop theroy

--- __GlowMint__/Js/_9th_control.js
+++ __GlowMint__/Js/_9th_control.js
@@ -1,1 +1,14 @@
-// control flow 
+/*
+What is a control statement in JavaScript?
+Control statements in JavaScript are statements that control the flow of execution in a program.
+They decide which code should execute, how many times it should execute, and when the program should move to another part of the code.
+
+Why / How are control statements used?
+Control statements are used to control how a program behaves based on different conditions or situations.
+Control statements allow a program to make decisions, repeat tasks, and change the normal flow of execution instead of simply running code from top to bottom.
+They are mainly used to:
+1. Make decisions using if, else, and switch.
+2. Repeat code using for, while, and do-while.
+3. Skip or stop loop execution using continue and break.
+
+*/
